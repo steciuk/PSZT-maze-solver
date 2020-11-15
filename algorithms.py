4e15d1a41109750ccac7b7e7462df25eeae8ab3f
strachus <steciuk77@gmail.com>
2020-11-15 23:25:33 +0100

    kurwa działa

--- algorithms.py
+++ algorithms.py
@@ -7,6 +7,7 @@ def breadth_first(maze, start, goal):
     and returns the shortest path from start to goal
     """
 
+    num = 0
     graph = maze.graph
     visited = []
     queue = deque([[start]])
@@ -15,17 +16,18 @@ def breadth_first(maze, start, goal):
         return "Start is the same as goal"
 
     while queue:
-        path = queue.pop()  # get path from the queue
+        path = queue.popleft()  # get path from the queue
         node = path[-1]  # get last cell from the path
         if node not in visited:
             neighbours = graph[node]
-
             for neighbour in neighbours:
+                num += 1
                 new_path = list(path)
                 new_path.append(neighbour)
                 queue.append(new_path)
 
                 if neighbour == goal:
+                    print(num)
                     return new_path
 
             visited.append(node)
@@ -34,14 +36,16 @@ def breadth_first(maze, start, goal):
 
 
 def depth_first(maze, start, goal):
+    num = 0
     graph = maze.graph
     path = [start]
     stack = deque([(path, start)])
     visited = set()
     while stack:
-        new_path, current = stack.pop()
+        path, current = stack.pop()
         if current == goal:
-            return new_path
+            print("checked nodes:", num)
+            return path
 
         if current in visited:
             continue
@@ -50,7 +54,9 @@ def depth_first(maze, start, goal):
         for neighbour in graph[current]:
             if neighbour in visited:
                 continue
-            append_path = list(new_path)
+
+            num += 1
+            append_path = list(path)
             append_path.append(neighbour)
             stack.append((append_path, neighbour))
     return "NO WAY!"
