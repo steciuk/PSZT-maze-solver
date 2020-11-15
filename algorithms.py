c5e1d2eb204f9f2c2f495b1490e3d663c60bf4d4
strachus <steciuk77@gmail.com>
2020-11-15 23:26:04 +0100

    Revert "kurwa działa"
    This reverts commit 4e15d1a41109750ccac7b7e7462df25eeae8ab3f.

--- algorithms.py
+++ algorithms.py
@@ -7,7 +7,6 @@ def breadth_first(maze, start, goal):
     and returns the shortest path from start to goal
     """
 
-    num = 0
     graph = maze.graph
     visited = []
     queue = deque([[start]])
@@ -16,18 +15,17 @@ def breadth_first(maze, start, goal):
         return "Start is the same as goal"
 
     while queue:
-        path = queue.popleft()  # get path from the queue
+        path = queue.pop()  # get path from the queue
         node = path[-1]  # get last cell from the path
         if node not in visited:
             neighbours = graph[node]
+
             for neighbour in neighbours:
-                num += 1
                 new_path = list(path)
                 new_path.append(neighbour)
                 queue.append(new_path)
 
                 if neighbour == goal:
-                    print(num)
                     return new_path
 
             visited.append(node)
@@ -36,16 +34,14 @@ def breadth_first(maze, start, goal):
 
 
 def depth_first(maze, start, goal):
-    num = 0
     graph = maze.graph
     path = [start]
     stack = deque([(path, start)])
     visited = set()
     while stack:
-        path, current = stack.pop()
+        new_path, current = stack.pop()
         if current == goal:
-            print("checked nodes:", num)
-            return path
+            return new_path
 
         if current in visited:
             continue
@@ -54,9 +50,7 @@ def depth_first(maze, start, goal):
         for neighbour in graph[current]:
             if neighbour in visited:
                 continue
-
-            num += 1
-            append_path = list(path)
+            append_path = list(new_path)
             append_path.append(neighbour)
             stack.append((append_path, neighbour))
     return "NO WAY!"
--- main.py
+++ main.py
@@ -2,13 +2,10 @@ from maze import Maze, Cell
 import algorithms
 
 # gen seeded 13x11 maze
-maze = Maze.from_file("maze.txt")
-print(maze.graph)
-#maze = Maze(10, 10, 0, 0, 2)
-start, end = maze.cell_at(0, 0), maze.cell_at(9, 9)
-#maze.path = algorithms.depth_first(maze, start, end)
-maze.path = algorithms.breadth_first(maze, start, end)
-print(maze.path)
+maze = Maze(80, 80, 0, 0, 2)
+start, end = maze.cell_at(0, 0), maze.cell_at(59, 59)
+maze.path = algorithms.depth_first(maze, start, end)
+
 # gen maze savestate
 # maze.generate_txt_save('maze.txt')
 
