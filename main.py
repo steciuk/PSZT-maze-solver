f61539fad555d1e6e2b864674c09d78c97f50b7e
strachus <steciuk77@gmail.com>
2020-11-15 23:43:06 +0100

    empty commit

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import algorithms
 # gen seeded 13x11 maze
 maze = Maze.from_file("maze.txt")
 print(maze.graph)
-#maze = Maze(10, 10, 0, 0, 2)
+# maze = Maze(10, 10, 0, 0, 2)
 start, end = maze.cell_at(0, 0), maze.cell_at(9, 9)
-#maze.path = algorithms.depth_first(maze, start, end)
+# maze.path = algorithms.depth_first(maze, start, end)
 maze.path = algorithms.breadth_first(maze, start, end)
 print(maze.path)
 # gen maze savestate
