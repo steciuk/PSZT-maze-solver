51f73d850933b31dba0bea72c87fd0d7b290db18
strachus <steciuk77@gmail.com>
2020-11-18 16:10:15 +0100

    wyświetlanie odwiedzanych nodeów w kolejności

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,18 @@
 from collections import deque
 from itertools import count
 
+
+def print_visited(visited, maze):
+    for y in range(maze.ny):
+        for x in range(maze.nx):
+            if maze.cell_at(x, y) in visited:
+                print(visited.index(maze.cell_at(x, y)), end=' ')
+            else:
+                print("X", end=' ')
+        print()
+    print()
+
+
 def breadth_first(maze, start, goal):
     """
     BFS algorithm, which keeps track of visited nodes of the graph
@@ -14,7 +26,7 @@ def breadth_first(maze, start, goal):
 
     while queue:
         path = queue.popleft()  # get path from the queue
-        node = path[-1]         # get last cell from the path
+        node = path[-1]  # get last cell from the path
         if node not in visited:
             neighbours = graph[node]
             for neighbour in neighbours:
@@ -28,10 +40,10 @@ def breadth_first(maze, start, goal):
 
                 if neighbour == goal:
                     print("Explored nodes: ", num)
+                    print_visited(visited, maze)
                     return new_path
 
             visited.append(node)
-
     return "There's no such path!"
 
 
@@ -40,17 +52,18 @@ def depth_first(maze, start, goal):
     graph = maze.graph
     path = [start]
     stack = deque([(path, start)])
-    visited = set()
+    visited = []
     while stack:
         path, current = stack.pop()
         if current == goal:
             print("Checked nodes:", num)
+            print_visited(visited, maze)
             return path
 
         if current in visited:
             continue
 
-        visited.add(current)
+        visited.append(current)
         for neighbour in graph[current]:
             if neighbour in visited:
                 continue
@@ -61,9 +74,10 @@ def depth_first(maze, start, goal):
             stack.append((append_path, neighbour))
     return "NO WAY!"
 
+
 def iter_deepening(maze, start, goal):
     for depth in range(maze.nx * maze.ny):
-        result, path = depth_limited(maze.graph, start, goal, depth)
+        result, path = depth_limited(maze, start, goal, depth)
         if result is False:
             continue
         print("Found a path to " + repr(goal) + ". Resulting depth: " + str(depth))
@@ -71,19 +85,23 @@ def iter_deepening(maze, start, goal):
 
     raise ValueError("There's no such path! (Reached maximum depth value)")
 
-def depth_limited(graph, start, goal, depth):
-    visited = set()
+
+def depth_limited(maze, start, goal, depth):
+    graph = maze.graph
+    visited = []
     stack = deque([[start]])
 
     while stack:
         path = stack.pop()
         node = path[-1]
         if node == goal:
+            print_visited(visited, maze)
             return True, path
         if depth <= 0:
+            print_visited(visited, maze)
             return False, []
 
-        visited.add(node)
+        visited.append(node)
         for neighbour in graph[node]:
             if neighbour in visited:
                 continue
--- main.py
+++ main.py
@@ -2,23 +2,22 @@ import time
 from maze import Maze
 import algorithms
 
-# gen seeded 13x11 maze
-# maze = Maze.from_file("maze.txt")
-maze = Maze(100, 100, 0, 0, seed=1, one_route=False, wall_knock=0.25)
+maze = Maze.from_file("pusty.txt")
+# maze = Maze(10, 10, 0, 0, seed=1, one_route=False, wall_knock=0.25)
 # print(maze.graph)
-start, end = maze.cell_at(0, 0), maze.cell_at(99, 99)
-t_start = time.process_time()
-maze.path = algorithms.depth_first(maze, start, end)
-t_end = time.process_time()
-print(t_end - t_start)
+start, end = maze.cell_at(0, 0), maze.cell_at(9, 9)
+# t_start = time.process_time()
+# maze.path = algorithms.depth_first(maze, start, end)
+# t_end = time.process_time()
+# print(t_end - t_start)
 
-t_start = time.process_time()
-maze.path = algorithms.breadth_first(maze, start, end)
-t_end = time.process_time()
-print(t_end - t_start)
+# t_start = time.process_time()
+# maze.path = algorithms.breadth_first(maze, start, end)
+# t_end = time.process_time()
+# print(t_end - t_start)
 
 # t_start = time.time()
-# maze.path = algorithms.iter_deepening(maze, start, end)
+maze.path = algorithms.iter_deepening(maze, start, end)
 # t_end = time.time()
 # print(t_end - t_start)
 # print(maze.path)
